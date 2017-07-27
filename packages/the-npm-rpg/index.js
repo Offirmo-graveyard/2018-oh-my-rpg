9e6cec5e0715e1728186a8f43468a6143887c16c
Offirmo <offirmo.net@gmail.com>
2017-07-27 22:40:06 +1000

    TLDW :scream_cat:

--- packages/the-npm-rpg/index.js
+++ packages/the-npm-rpg/index.js
@@ -12,6 +12,7 @@ const en_adventures = require('@oh-my-rpg/data/src/adventure_archetype/i18n').en
 const prettifyJson = require('@offirmo/cli-toolbox/string/prettify-json')
 const boxify = require('@offirmo/cli-toolbox/string/boxify')
 const stylizeString = require('@offirmo/cli-toolbox/string/stylize')
+const linewrap = require('@offirmo/cli-toolbox/string/linewrap')
 //const json = require('@offirmo/cli-toolbox/fs/json')
 //const arrayify = require('@offirmo/cli-toolbox/string/arrayify')
 //const columnify = require('@offirmo/cli-toolbox/string/columnify')
@@ -33,6 +34,8 @@ const {
 } = require('@oh-my-rpg/view-text')
 
 const { version } = require('./package.json')
+const MINIMAL_TERMINAL_WIDTH = 80
+const MANY_SPACES = '                                                                                                                                                      '
 
 let verbose = false
 
@@ -44,8 +47,11 @@ Globalize.loadMessages({en: en_adventures})
 
 //console.log(boxify('𝐓he 𝐁oring 𝐑𝐏𝐆 𝑟𝑒𝑙𝑜𝑎𝑑𝑒𝑑 ', {padding: 1, margin: 1, borderStyle: 'double'}))
 //console.log(boxify('𝐓𝐡𝐞 𝐁𝐨𝐫𝐢𝐧𝐠 𝐑𝐏𝐆 𝑟𝑒𝑙𝑜𝑎𝑑𝑒𝑑 ', {padding: 2, margin: 1, borderStyle: 'double', borderColor: 'red'}))
-console.log(boxify(`   ${stylizeString.bold('The Boring RPG 𝑟𝑒𝑙𝑜𝑎𝑑𝑒𝑑')} \n\nhttp://www.online-adventur.es`, {padding: 1, margin: 0, borderStyle: 'double', borderColor: 'red'}))
-console.log(`v${version}`)
+console.log(
+	stylizeString.bold('The npm RPG')
+	+ ` - v${version} - `
+	+ stylizeString.underline('http://www.online-adventur.es/the-npm-rpg')
+)
 
 const config = new Conf({
 	configName: 'state',
@@ -88,31 +94,64 @@ const rendering_options = {
 	last_adventure: state.last_adventure
 }
 
+console.log(
+	boxify(''
+		+ ('You continue your adventures...' + MANY_SPACES).slice(0, MINIMAL_TERMINAL_WIDTH - 4) + '\n\n'
+		+ linewrap(MINIMAL_TERMINAL_WIDTH - 4)(''
+			//+ `Click #${state.good_click_count}\n\n`
+			+ stylizeString.bold(render_adventure(state.last_adventure, rendering_options))
+		),
+		{
+			padding: 1,
+			borderStyle: 'double',
+			borderColor: 'red'
+		}
+	)
+)
 
-console.log(stylizeString.bold(`\n============ Click #${state.good_click_count} ============\n`))
-console.log(render_adventure(state.last_adventure, rendering_options))
+function boxifyAlt(position, s) {
+	const lines = s.split('\n')
+	lines[0] = (lines[0] + MANY_SPACES).slice(0, MINIMAL_TERMINAL_WIDTH - 2)
+	const boxified = boxify(lines.join('\n'))
+	const boxified_lines = boxified.split('\n').slice(
+		(position !== 'top') ? 1 : 0
+	)
+	if (position !== 'bottom')
+		boxified_lines[boxified_lines.length - 1] =
+			boxified_lines[boxified_lines.length - 1]
+			.replace('└', '├')
+			.replace('┘', '┤')
 
-function noboxify(s) { return '\n' + s }
+	return boxified_lines.join('\n')
+}
 
-console.log(noboxify(
-	stylizeString.bold('🙂  CHARACTERISTICS 💗\n')
+console.log(boxifyAlt('top',
+	//stylizeString.bold('🙂  CHARACTERISTICS 💗\n')
+	stylizeString.bold('CHARACTERISTICS:\n')
 	+ render_characteristics(state.characteristics, rendering_options),
 	{borderStyle: 'single'}
 ))
-console.log(noboxify(
-	stylizeString.bold('⚔  ACTIVE EQUIPMENT 🛡 \n')
+console.log(boxifyAlt('middle',
+	//stylizeString.bold('⚔  ACTIVE EQUIPMENT 🛡 \n')
+	stylizeString.bold('ACTIVE EQUIPMENT:\n')
 	+ render_equipment(state.inventory, rendering_options),
 	{borderStyle: 'single'}
 ))
-console.log(noboxify(
-	stylizeString.bold('📦  INVENTORY 💰 \n')
+console.log(boxifyAlt('bottom',
+	//stylizeString.bold('📦  INVENTORY 💰 \n')
+	stylizeString.bold('INVENTORY:\n')
 	+ render_wallet(state.wallet, rendering_options)
 	+ '\n'
 	+ render_inventory(state.inventory, rendering_options),
 	{borderStyle: 'single'}
 ))
 
-console.log('You can play again in...')
+//console.log('You can play again in...')
+console.log('\n')
+console.log(stylizeString.bold(`       That was your adventure #${state.good_click_count}!`))
+console.log(stylizeString.bold('👉 👉 👉  Relaunch the-npm-rpg to continue your adventures! ⚔ 💰 🎁  👈 👈 👈 '))
+console.log(stylizeString.bold('       You will get stronger and stronger!'))
+console.log('\n')
 
 // TODO print advices (equip, sell...)
 config.set(state)
