acedefb6a0bebbfa8455b28fe48b2c143e4c693d
Offirmo <offirmo.net@gmail.com>
2018-01-09 09:19:17 +1100

    TLDW :scream_cat:

--- packages/logic-monsters/dist/src.es7.cjs/data/index.d.ts
+++
@@ -1,6 +0,0 @@
-interface RawMonsterEntry {
-    name: string;
-    emoji: string;
-}
-declare const ENTRIES: RawMonsterEntry[];
-export { RawMonsterEntry, ENTRIES };
--- packages/soft-execution-context/dist/src.es7.cjs/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isomorphic = require("./soft-execution-context");
-exports.isomorphic = isomorphic;
-//# sourceMappingURL=index.js.map
